135851272a5803d4e5f5265fef73cd5963819a31
RashidPanhwar <rashidpanhwer8@gmail.com>
2024-09-09 02:43:34 +0500

    import seted

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
-import CompExperties from '@/components/comprihence-experties/CompExperties'
-import Footer from '@/components/footer/Footer'
-import Hero from '@/components/hero/Hero'
-import Inspiration from '@/components/inspiration/Inspiration'
-import MyLocation from '@/components/location/Location'
-import Navbar from '@/components/navbar/Navbar'
-import Services from '@/components/services/Services'
-import Offers from '@/components/what-we-offer/Offers'
+import React from 'react';
+import CompExperties from '@/components/comprihence-experties/CompExperties';
+import Footer from '@/components/footer/Footer';
+import Hero from '@/components/hero/Hero';
+import Inspiration from '@/components/inspiration/Inspiration';
+import MyLocation from '@/components/location/Location';
+import Navbar from '@/components/navbar/Navbar';
+import Services from '@/components/services/Services';
+import Offers from '@/components/what-we-offer/Offers';
 
 const FrontPage = () => {
+  // Debugging logs
+  console.log('CompExperties:', CompExperties);
+  console.log('Footer:', Footer);
+  console.log('Hero:', Hero);
+  console.log('Inspiration:', Inspiration);
+  console.log('MyLocation:', MyLocation);
+  console.log('Navbar:', Navbar);
+  console.log('Services:', Services);
+  console.log('Offers:', Offers);
+
   return (
     <div>
-      
-        <Navbar />
-        <section id='hero'>
-          <Hero />
-        </section>
-        <section id='inspire'>
-          <Inspiration />
-        </section>
-        <section id='experties'>
-          <CompExperties />
-        </section>
-        <section id='services'>
-          <Services />
-        </section>
-        <section id='offers'>
-          <Offers />
-        </section>
-        <section id='location'>
-          <MyLocation />
-        </section>
-        <Footer />
+      <Navbar />
+      <section id='hero'>
+        <Hero />
+      </section>
+      <section id='inspire'>
+        <Inspiration />
+      </section>
+      <section id='experties'>
+        <CompExperties />
+      </section>
+      <section id='services'>
+        <Services />
+      </section>
+      <section id='offers'>
+        <Offers />
+      </section>
+      <section id='location'>
+        <MyLocation />
+      </section>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default FrontPage
+export default FrontPage;
