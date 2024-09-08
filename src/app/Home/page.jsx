cb59f1e494c41195c38662f1f40056967f1aafe8
RashidPanhwar <rashidpanhwer8@gmail.com>
2024-09-09 02:13:38 +0500

    set error

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,18 +1,12 @@
 import CompExperties from '@/components/comprihence-experties/CompExperties'
-import Courses from '@/components/cources/Courses'
-import DiscountBanner from '@/components/discount-banner/DiscountBanner'
 import Footer from '@/components/footer/Footer'
 import Hero from '@/components/hero/Hero'
 import Inspiration from '@/components/inspiration/Inspiration'
 import MyLocation from '@/components/location/Location'
 import Navbar from '@/components/navbar/Navbar'
 import Services from '@/components/services/Services'
-import Testimonials from '@/components/testimonials/Testimonials'
 import Offers from '@/components/what-we-offer/Offers'
-// import { Inspiration } from 'next/font/google'
 import React from 'react'
-// import Navbar from '../components/navbar/Navbar'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const FrontPage = () => {
   return (
